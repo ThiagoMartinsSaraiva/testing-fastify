3cc748333337e7000fa847bc7cbab75c4612d7c0
ThiagoMartinsSaraiva <thiagomartinssaraiva@gmail.com>
2020-01-27 23:08:58 -0300

    routes

--- routes.js
+++ routes.js
@@ -2,6 +2,24 @@ async function routes (fastify, options) {
   fastify.get('/', async (req, reply) => {
     reply.send('API is running')
   })
+
+  fastify.get('/shinobi', async (req, reply) => {
+    reply.send('This is the list of shinobis')
+  })
+
+  fastify.get('/shinobi/:id', async (req, reply) => {
+    reply.send(`This is the shinobi of id ${req.params.id}`)
+  })
+
+  fastify.post('/shinobi', async (req, reply) => {
+    console.log(req.body)
+    reply.send(req.body)
+  })
+
+  fastify.delete('/shinobi/:id', async (req, reply) => {
+    console.log(req.params.id)
+    reply.send('Deleted successfully')
+  })
 }
 
 module.exports = routes
